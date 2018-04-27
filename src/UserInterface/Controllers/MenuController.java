88dbcc62c642ae45446515e808b16d46738621e3
KonradMagiera <k.magiera1@wp.pl>
2018-04-27 19:35:41 +0200

    controls for generation

--- src/UserInterface/Controllers/MenuController.java
+++ src/UserInterface/Controllers/MenuController.java
@@ -4,10 +4,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.GridPane;
 
 
 public class MenuController implements Initializable {
@@ -18,6 +20,16 @@ public class MenuController implements Initializable {
     private Label currentGen;
     @FXML
     private Slider speedSlider;
+    @FXML
+    private GridPane menuPane;
+    @FXML
+    private Button backwardB;
+    @FXML
+    private Button pauseB;
+    @FXML
+    private Button playB;
+    @FXML
+    private Button forwardB;
 
 
     @Override
