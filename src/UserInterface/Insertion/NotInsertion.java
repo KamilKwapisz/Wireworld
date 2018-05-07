c2d3b3969e200da11876d32eefbafa181959f116
KonradMagiera <k.magiera1@wp.pl>
2018-05-07 19:57:30 +0200

    Not logic gate

---
+++ src/UserInterface/Insertion/NotInsertion.java
@@ -0,0 +1,107 @@
+package UserInterface.Insertion;
+
+import UserInterface.Controllers.GameGrid;
+
+public class NotInsertion implements Insertion{
+
+    @Override
+    public void insertTop(GameGrid.Tile[][] grid, int x, int y) {
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+    }
+
+    @Override
+    public void insertBottom(GameGrid.Tile[][] grid, int x, int y) {
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+    }
+
+    @Override
+    public void insertLeft(GameGrid.Tile[][] grid, int x, int y) {
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+    }
+
+    @Override
+    public void insertRight(GameGrid.Tile[][] grid, int x, int y) {
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+        grid[x][y].yellowTile();
+    }
+    
+}
