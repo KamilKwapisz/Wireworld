7eaddf8c924c026d013c243cc725775bf68ee9e3
KonradMagiera <k.magiera1@wp.pl>
2018-05-14 17:15:42 +0200

    change grid bug fix 0.1

--- src/UserInterface/Controllers/MainController.java
+++ src/UserInterface/Controllers/MainController.java
@@ -257,8 +257,12 @@ public class MainController implements Initializable {
             wireDisplay.getChildren().clear();
             game = small;
             wireDisplay.setCenter(game.createContent());
+            
+            simulation.pause();
+            simulation.setNewGrid(game);
+            simulation.initializeBoardFromGrid();
         }
-        simulation.setNewGrid(game);
+        
     }
 
     @FXML
@@ -275,8 +279,12 @@ public class MainController implements Initializable {
             wireDisplay.getChildren().clear();
             game = medium;
             wireDisplay.setCenter(game.createContent());
+            
+            simulation.pause();
+            simulation.setNewGrid(game);
+            simulation.initializeBoardFromGrid();
         }
-        simulation.setNewGrid(game);
+        
     }
 
     @FXML
@@ -293,8 +301,12 @@ public class MainController implements Initializable {
             wireDisplay.getChildren().clear();
             game = large;
             wireDisplay.setCenter(game.createContent());
+            
+            simulation.pause();
+            simulation.setNewGrid(game);
+            simulation.initializeBoardFromGrid();
         }
-        simulation.setNewGrid(game);
+        
     }
 
     public GameGrid getGame() {
