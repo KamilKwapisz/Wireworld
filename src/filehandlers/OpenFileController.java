41aafbaaf753a7d9935d1ac2d87c9883cdd4c27e
KonradMagiera <k.magiera1@wp.pl>
2018-05-31 06:48:21 +0200

    error message when there are invalid pixel colors in opened picture

--- src/filehandlers/OpenFileController.java
+++ src/filehandlers/OpenFileController.java
@@ -13,26 +13,32 @@ import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
-
+import userinterface.dialogwindows.ErrorWindow;
+import userinterface.dialogwindows.PopUpWindow;
 
 public class OpenFileController implements FileController {
+
     private Image image;
     private FileChooser fileChooser;
     private PixelReader reader;
+    private File openedFile;
 
-    public OpenFileController(GameGrid grid, Stage stage){
+    public OpenFileController(GameGrid grid, Stage stage) {
         createFileChooser(stage);
-        try{
+        try {
             reader = image.getPixelReader();
-            loadToGrid(grid);
-        } catch(NullPointerException e){
+            if (loadToGrid(grid)) {
+                PopUpWindow errorWindow = new ErrorWindow("Invalid generation " + openedFile.getName() + ".\nThere are pixels with unsupported colors\nin the picture.");
+                errorWindow.display();
+            }
+        } catch (NullPointerException e) {
             System.out.println("File was not chosen.");
             reader = null;
         }
     }
 
     @Override
-    public void createFileChooser(Stage stage){
+    public void createFileChooser(Stage stage) {
         fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("PNG files", "*.png"),
@@ -40,33 +46,36 @@ public class OpenFileController implements FileController {
         fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
         fileChooser.setTitle("Load generation");
         fileChooser.setInitialFileName("generation");
-        File openedFile = fileChooser.showOpenDialog(stage);
-        if(openedFile != null){
-            try{
+        openedFile = fileChooser.showOpenDialog(stage);
+        if (openedFile != null) {
+            try {
                 BufferedImage bImage = ImageIO.read(openedFile);
                 image = SwingFXUtils.toFXImage(bImage, null);
-            }catch(IOException e){
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
-    private void loadToGrid(GameGrid grid){
-        for(int x = 0; x < grid.getXTiles() && x < image.getWidth(); x++){
-            for(int y = 0; y < image.getHeight() && y < grid.getYTiles(); y++){
+    private boolean loadToGrid(GameGrid grid) {
+        boolean wrongColorAppeared = false;
+        for (int x = 0; x < grid.getXTiles() && x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight() && y < grid.getYTiles(); y++) {
                 Color color = reader.getColor(x, y);
-                if(color.equals(Color.BLACK)){
+                if (color.equals(Color.BLACK)) {
                     grid.changeState(x, y, CellType.EMPTY);
-                } else if(color.equals(Color.YELLOW)){
+                } else if (color.equals(Color.YELLOW)) {
                     grid.changeState(x, y, CellType.CONDUCTOR);
-                } else if(color.equals(Color.BLUE)){
+                } else if (color.equals(Color.BLUE)) {
                     grid.changeState(x, y, CellType.HEAD);
-                } else if(color.equals(Color.RED)){
+                } else if (color.equals(Color.RED)) {
                     grid.changeState(x, y, CellType.TAIL);
-                } else{
+                } else {
                     grid.changeState(x, y, CellType.EMPTY);
+                    wrongColorAppeared = true;
                 }
             }
         }
+        return wrongColorAppeared;
     }
 }
